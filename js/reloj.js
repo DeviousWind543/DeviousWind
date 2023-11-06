b347d51dd892e3a413f797f5119761cd4db3ceda
DeviousWind543 <98923520+DeviousWind543@users.noreply.github.com>
2023-11-06 15:51:55 -0500

    Version 0.0.1

--- js/reloj.js
+++ js/reloj.js
@@ -1,11 +1,21 @@
 function actualizarReloj() {
+    const reloj = document.getElementById('reloj');
     const fecha = new Date();
-    const horas = fecha.getHours().toString().padStart(2, '0');
-    const minutos = fecha.getMinutes().toString().padStart(2, '0');
-    const segundos = fecha.getSeconds().toString().padStart(2, '0');
-    const horaActual = `${horas}:${minutos}:${segundos}`;
-    
-    document.getElementById("reloj").innerHTML = horaActual;
+    const horas = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const segundos = fecha.getSeconds();
+
+    // Formatea las horas, minutos y segundos con ceros a la izquierda si son menores de 10
+    const horaFormateada = horas < 10 ? `0${horas}` : horas;
+    const minutosFormateados = minutos < 10 ? `0${minutos}` : minutos;
+    const segundosFormateados = segundos < 10 ? `0${segundos}` : segundos;
+
+    // Actualiza el contenido del elemento con la hora
+    reloj.textContent = `${horaFormateada}:${minutosFormateados}:${segundosFormateados}`;
 }
 
-setInterval(actualizarReloj, 1000); // Actualiza el reloj cada segundo (1000 ms)
+// Llama a la función para actualizar el reloj cada segundo
+setInterval(actualizarReloj, 1000);
+
+// Llama a la función para actualizar el reloj cuando la página se carga
+window.addEventListener('load', actualizarReloj);
